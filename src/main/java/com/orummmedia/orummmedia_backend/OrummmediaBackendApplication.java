819f81b6812237aee8c7d78548fbd5f7f17f15f9
pshoon25 <pshoon25@naver.com>
2024-08-06 22:28:27 +0900

    cors fix

--- src/main/java/com/orummmedia/orummmedia_backend/OrummmediaBackendApplication.java
+++ src/main/java/com/orummmedia/orummmedia_backend/OrummmediaBackendApplication.java
@@ -14,14 +14,15 @@ public class OrummmediaBackendApplication {
 	}
 
 	@Bean
-	public WebMvcConfigurer corsConfigure(){
+	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000", "http://www.rs-nova.co.kr")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
-						.allowedMethods("*")
-						.allowedOrigins("http://localhost:3000", "http://www.rs-nova.co.kr");
+						.allowCredentials(true); // credentials 지원 여부에 따라 설정
 			}
 		};
 	}
