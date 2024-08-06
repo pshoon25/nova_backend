7403cab9188ad10d0b2857d9710d1d4079041b3e
pshoon25 <pshoon25@naver.com>
2024-08-06 22:36:03 +0900

    cors fix

--- src/main/java/com/orummmedia/orummmedia_backend/OrummmediaBackendApplication.java
+++ src/main/java/com/orummmedia/orummmedia_backend/OrummmediaBackendApplication.java
@@ -21,7 +21,7 @@ public class OrummmediaBackendApplication {
 				registry.addMapping("/**")
 						.allowedOrigins("http://localhost:3000", "http://www.rs-nova.co.kr")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
+						.allowedHeaders("Access-Control-Allow-Origin")
 						.allowCredentials(true);
 			}
 		};
