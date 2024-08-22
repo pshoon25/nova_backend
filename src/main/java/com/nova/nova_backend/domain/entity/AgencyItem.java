deba49c903ab0573cc449e8fda2b24edbd9b9467
박성훈 <116499170+pshoon25@users.noreply.github.com>
2024-08-22 09:13:34 +0900

    Create AgencyItem.java

---
+++ src/main/java/com/nova/nova_backend/domain/entity/AgencyItem.java
@@ -0,0 +1,40 @@
+package com.nova.nova_backend.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "T_AGENCY_ITEM")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AgencyItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ITEM_SEQ", nullable = false)
+    private Long itemSeq;
+
+    @ManyToOne
+    @JoinColumn(name = "AGENCY_CODE", nullable = false)
+    private Agency agency;
+
+    @Column(name = "ITEM_NAME", length = 50, nullable = false)
+    private String itemName;
+
+    @Column(name = "ITEM_PRICE", nullable = false)
+    private BigDecimal itemPrice;
+
+    @Column(name = "USE_YN", length = 1, nullable = false)
+    private String useYn;
+}
