d537f0eab363f5960f6e6362261f9c1eb0468dce
박성훈 <116499170+pshoon25@users.noreply.github.com>
2024-08-22 09:52:47 +0900

    Update AgencyPointRepository.java

--- src/main/java/com/nova/nova_backend/repository/AgencyPointRepository.java
+++ src/main/java/com/nova/nova_backend/repository/AgencyPointRepository.java
@@ -9,9 +9,9 @@ import com.nova.nova_backend.domain.dto.AgencyPointDTO;
 import java.util.List;
 
 public interface AgencyPointRepository extends JpaRepository<AgencyPoint, Long> {
-    @Query("SELECT new com.nova.nova_backend.domain.dto.AgencyPointDTO(p.availablePoints) " +
-            "FROM AgencyPoint p " +
-            "LEFT JOIN Agency a " +
-            "WHERE a.agencyName LIKE %:agencyName%")
-    List<AgencyPointDTO> findPointsByAgencyName(@Param("agencyName") String agencyName);
+    // @Query("SELECT new com.nova.nova_backend.domain.dto.AgencyPointDTO(p.availablePoints) " +
+    //         "FROM AgencyPoint p " +
+    //         "LEFT JOIN Agency a " +
+    //         "WHERE a.agencyName LIKE %:agencyName%")
+    // List<AgencyPointDTO> findPointsByAgencyName(@Param("agencyName") String agencyName);
 }
