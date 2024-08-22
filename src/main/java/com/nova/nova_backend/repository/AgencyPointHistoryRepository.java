edf8c36e8df5daa1edc86abace0175e16fc8269b
박성훈 <116499170+pshoon25@users.noreply.github.com>
2024-08-22 09:18:17 +0900

    Update AgencyPointHistoryRepository.java

--- src/main/java/com/nova/nova_backend/repository/AgencyPointHistoryRepository.java
+++ src/main/java/com/nova/nova_backend/repository/AgencyPointHistoryRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface AgencyPointHistoryRepository extends JpaRepository<AgencyPointHistory, Long> {
-    @Query("SELECT new com.nova.nova_backend.domain.dto.PointHistoryDTO(p.pointHistoryNo, a.agencyName, p.mission.missionNo, p.content, p.points, p.registerDateTime, p.status) " +
+    @Query("SELECT new com.nova.nova_backend.domain.dto.PointHistoryDTO(p.pointHistoryNo, a.agencyName, m.missionNo, p.content, p.points, p.registerDateTime, p.status) " +
             "FROM AgencyPointHistory p " +
             "LEFT JOIN p.agency a " +
             "LEFT JOIN p.mission m " +
@@ -17,5 +17,4 @@ public interface AgencyPointHistoryRepository extends JpaRepository<AgencyPointH
             "(:status IS NULL OR p.status = :status)")
     List<PointHistoryDTO> findPointHistoryDetails(@Param("agencyName") String agencyName,
                                                   @Param("status") String status);
-
 }
