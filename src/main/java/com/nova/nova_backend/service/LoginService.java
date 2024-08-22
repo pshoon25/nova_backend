46b8eb37bb4c399bf17d9ea42c2e4765f4ab89b9
박성훈 <116499170+pshoon25@users.noreply.github.com>
2024-08-22 09:55:12 +0900

    Update LoginService.java

--- src/main/java/com/nova/nova_backend/service/LoginService.java
+++ src/main/java/com/nova/nova_backend/service/LoginService.java
@@ -33,7 +33,7 @@ public class LoginService {
         Map<String, Object> result = new HashMap<>();
 
         // 로그인 아이디로 정보 조회
-        Agency resultAgency = null; //agencyRepository.findByLoginId(loginId);
+        Agency resultAgency =agencyRepository.findByLoginId(loginId);
 
         if (resultAgency == null) {
             result.put("failed", "Id Failed");
