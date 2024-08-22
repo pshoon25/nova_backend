55232d693dde4809eb2dfce783a21cb93a15bbbb
박성훈 <116499170+pshoon25@users.noreply.github.com>
2024-08-22 09:21:32 +0900

    Update AgencyItem.java

--- src/main/java/com/nova/nova_backend/domain/entity/AgencyItem.java
+++ src/main/java/com/nova/nova_backend/domain/entity/AgencyItem.java
@@ -9,6 +9,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
