b7c41d84b87ee0524fc301a35308d8daef35c609
pshoon25 <pshoon25@naver.com>
2024-08-29 00:39:03 +0900

    UPDATE

--- src/main/java/com/nova/nova_backend/config/WebConfig.java
+++ src/main/java/com/nova/nova_backend/config/WebConfig.java
@@ -16,7 +16,8 @@ public class WebConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000", "http://www.rs-nova.co.kr", "https://www.rs-nova.co.kr")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                        .allowedHeaders("Gmt-Time", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Authorization", "Cache-Control", "Access-Control-Allow-Origin", "Access-Token")
+                        .allowedHeaders("*")
+                        //.allowedHeaders("Gmt-Time", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Authorization", "Cache-Control", "Access-Control-Allow-Origin", "Access-Token")
                         .allowCredentials(true)
                         .maxAge(3600);
             }
